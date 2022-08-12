427ea1f946db1ab0852db9da284af46706bbb49e
Ifeanyi Okwuobi <fredrickokwuobi@gmail.com>
2022-08-12 16:04:53 +0100

    changes tony made

--- article.js
+++ article.js
@@ -1,3 +1,4 @@
 // this is a pretty good article.
 // Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur illo et suscipit hic ipsa saepe totam reprehenderit doloribus? Deleniti at repellat iure quas eaque deserunt nobis, autem voluptatibus dolorem sint?
 
+// changes made by tony the Mentor
