84f979e67c336e9d7903c1581afe6534a5fa2d49
Ifeanyi Okwuobi <fredrickokwuobi@gmail.com>
2022-08-12 16:01:46 +0100

    made changes to article file

--- article.js
+++ article.js
@@ -1,1 +1,3 @@
 // this is a pretty good article.
+// Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur illo et suscipit hic ipsa saepe totam reprehenderit doloribus? Deleniti at repellat iure quas eaque deserunt nobis, autem voluptatibus dolorem sint?
+
